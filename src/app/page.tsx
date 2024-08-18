998e1d110ed5233c0aaf58cdb7b1806153486426
wjdsheep15 <wjd15sheep@gmail.com>
2024-08-18 15:59:14 +0900

    Feat: project first setting

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='flex h-10 w-10'>우리 다함께 놀아요</div>
+    </main>
+  )
+}
