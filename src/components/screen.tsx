a3765033ed8534edf798502ffff2bda8caebe60c
wjdsheep15 <wjd15sheep@gmail.com>
2024-08-30 16:47:30 +0900

    feat#4 mainpage add  DynamicLayout

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -1,3 +1,7 @@
 export default function Screen({ children }: { children: React.ReactNode }) {
-  return <div className='flex justify-center px-44 py-10'>{children}</div>
+  return (
+    <div className='flex items-center justify-center px-5 py-10 sm:px-12 md:px-16 lg:px-28 xl:px-36 2xl:px-44'>
+      {children}
+    </div>
+  )
 }
