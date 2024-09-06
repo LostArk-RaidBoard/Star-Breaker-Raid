1b745696280649dde69c238570d30eb6c6c53df2
wjdsheep15 <wjd15sheep@gmail.com>
2024-09-06 20:19:12 +0900

    fix post icon

--- src/components/MainField/MainWePosts.tsx
+++ src/components/MainField/MainWePosts.tsx
@@ -1,20 +1,41 @@
+import Image from 'next/image'
+import User from '@image/icon/user.svg'
+import Hand from '@image/icon/hand.svg'
+import Clock from '@image/icon/clock.svg'
+import Fire from '@image/icon/fire.svg'
+import Megaphone from '@image/icon/megaphone.svg'
+
 export default function MainWePosts() {
   return (
     <div className='h-full w-full rounded-md bg-gray-300 shadow-lg md:w-[45%]'>
-      <div className='grid grid-cols-8 rounded-t-md bg-gray-200'>
-        <div className='col-span-2 flex items-center justify-center'>레이드</div>
-        <div className='col-span-2 flex items-center justify-center'>공대장 캐릭터</div>
-        <div className='col-span-2 flex items-center justify-center'>시간</div>
-        <div className='flex items-center justify-center'>인원</div>
-        <div className='flex items-center justify-center'>신청 버튼</div>
+      <div className='grid grid-cols-8 rounded-t-md bg-gray-200 p-1'>
+        <div className='col-span-2 flex items-center justify-center gap-1'>
+          <Fire className='h-4 w-4' />
+          레이드
+        </div>
+        <div className='col-span-2 flex items-center justify-center gap-1'>
+          <Megaphone className='h-4 w-4' />
+          공대장
+        </div>
+        <div className='col-span-2 flex items-center justify-center gap-1'>
+          <Clock className='h-4 w-4' />
+          시간
+        </div>
+        <div className='flex items-center justify-center gap-1'>
+          <User className='h-4 w-4' />
+          인원
+        </div>
+        <div className='flex items-center justify-center gap-1'>
+          <Hand className='h-4 w-4' /> 신청
+        </div>
       </div>
       <div className='mt-2 flex w-full flex-col gap-3 p-1'>
         <div className='grid h-9 grid-cols-8 rounded-md border border-gray-900 bg-gray-100 p-1'>
           <div className='col-span-2 flex items-center justify-center border-r border-gray-500'>
-            애키르 하드
+            에키르 하드
           </div>
           <div className='col-span-2 flex w-full flex-nowrap items-center justify-center gap-1 truncate border-r border-gray-500'>
-            <img
+            <Image
               src='/classIcon/브레이커.svg'
               alt='아이콘'
               width={100}
