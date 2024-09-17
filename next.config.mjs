386c3af5a2aeb2e78dc19821c30f09fcd2d92918
wjdsheep15 <wjd15sheep@gmail.com>
2024-09-17 10:36:55 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -25,9 +25,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    POSTGRES_URL: process.env.POSTGRES_URL,
-  },
 }
 
 export default nextConfig
--- src/app/api/auth/[... nextauth]/route.ts
+++ src/app/api/auth/[... nextauth]/route.ts
@@ -2,19 +2,19 @@ import NextAuth from 'next-auth'
 import { handlers } from '@/auth' // Referring to the auth.ts we just created
 import GoogleProvider from 'next-auth/providers/google'
 
-// const authOption = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//   ],
-//   secret: process.env.AUTH_SECRET,
-// }
+const authOption = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+}
 
-// const handler = NextAuth(
-//   //NextAuth.js를 사용하여 인증과 관련된 작업을 처리하는 핵심 함수이다.
-//   authOption,
-// )
+const handler = NextAuth(
+  //NextAuth.js를 사용하여 인증과 관련된 작업을 처리하는 핵심 함수이다.
+  authOption,
+)
 
 export const { GET, POST } = handlers
