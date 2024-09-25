26b7d75c4a4fbbd1841b930713695a4a3037a248
wjdsheep15 <wjd15sheep@gmail.com>
2024-09-26 02:50:26 +0900

    fix mainPage post

--- src/components/MainField/MainPost.tsx
+++ src/components/MainField/MainPost.tsx
@@ -2,12 +2,101 @@ import MainCharacter from '@/components/MainField/MainCharacter'
 import MainTeacherPosts from '@/components/MainField/MainTeacherPost'
 import MainWePosts from '@/components/MainField/MainWePosts'
 
-export default function MainPost() {
+interface RaidPost {
+  post_id: number
+  raid_name: string
+  raid_time: any
+  limit_level: number
+  user_id: string
+  post_position: string
+  noti: string
+  fixed: boolean
+  character_level: string
+  character_name: string
+  raid_limitperson: number
+  raid_type: string
+  raid_maxtime: string
+  character_classicon: string
+}
+
+async function fetchTeacherPosts(): Promise<RaidPost[]> {
+  try {
+    const response = await fetch(`${process.env.API_URL}/api/raidPostGet?posts_position=teacher`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    if (response.ok) {
+      return data.postRows // 데이터를 반환
+    }
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+  return [] // 오류 발생 시 빈 배열 반환
+}
+
+const fetchWePostsFetch = async (): Promise<RaidPost[]> => {
+  try {
+    const response = await fetch(`${process.env.API_URL}/api/raidPostGet?posts_position=user`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    if (response.ok && response.status === 201) {
+      return data.postRows
+    }
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+  return []
+}
+
+async function fetchApplicationsCount(postsRows: RaidPost[]): Promise<{ [key: number]: number }> {
+  const counts: { [key: number]: number } = {}
+  const promises = postsRows.map(async (item) => {
+    try {
+      const response = await fetch(
+        `${process.env.API_URL}/api/applicationCount?post_id=${item.post_id}`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      if (response.ok) {
+        const data = await response.json()
+        counts[item.post_id] = data.count + 1 || 0
+      } else {
+        counts[item.post_id] = 1
+      }
+    } catch (error) {
+      console.error(error)
+      counts[item.post_id] = 1 // 오류 발생 시 기본값 설정
+    }
+  })
+
+  await Promise.all(promises)
+  return counts // 모든 카운트가 포함된 객체 반환
+}
+
+export default async function MainPost() {
+  const postsTeacherRows = await fetchTeacherPosts() // 포스트 데이터 가져오기
+  const postsWeRows = await fetchWePostsFetch()
+  const applicationsCount = await fetchApplicationsCount(postsTeacherRows) // 카운트 데이터 가져오기
+  const weApplicationsCount = await fetchApplicationsCount(postsWeRows)
+
   return (
     <div className='flex h-full w-full flex-col gap-4 md:flex-row'>
       <MainCharacter />
-      <MainTeacherPosts />
-      <MainWePosts />
+      <MainTeacherPosts teacherPostsRows={postsTeacherRows} applicationsCount={applicationsCount} />
+      <MainWePosts wePostsRows={postsWeRows} applicationsCount={weApplicationsCount} />
     </div>
   )
 }
