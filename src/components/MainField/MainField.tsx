bc72dcd9020ed5aae935c353c868111e613c461e
wjdsheep15 <wjd15sheep@gmail.com>
2024-09-27 20:13:14 +0900

    Dev version 0.4.7

--- src/components/MainField/MainField.tsx
+++ src/components/MainField/MainField.tsx
@@ -6,7 +6,7 @@ export default function MainField() {
   return (
     <div className='mt-8 flex h-full w-full flex-col items-center justify-center'>
       <div className='flex h-[1000px] w-full flex-col gap-4 md:h-[400px] md:flex-row'>
-        <MainPost />
+        {/* <MainPost /> */}
       </div>
       <div className='mt-8 h-24 w-full'>
         <SiteLink />
