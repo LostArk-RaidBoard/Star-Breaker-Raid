4ab50b30847cab4969a2966bb1cd7ead196c2d30
wjdsheep15 <wjd15sheep@gmail.com>
2024-09-27 23:25:47 +0900

    fix version 0.4.8.26

--- src/components/utils/formatDate.ts
+++ src/components/utils/formatDate.ts
@@ -1,14 +1,18 @@
 export default function FormatDate(dateString: string) {
-  // 유효한 날짜인지 확인
   if (!dateString) return 'Invalid Date'
 
   const date = new Date(dateString)
+
+  // 날짜가 유효하지 않은 경우
   if (isNaN(date.getTime())) {
     return 'Invalid Date'
   }
+
+  console.log('Parsed date:', date) // 여기에서 date 객체를 확인합니다.
+  // 한국 시간으로 변환
   const optionsDate: Intl.DateTimeFormatOptions = {
     weekday: 'short',
-    timeZone: 'Asia/Seoul', // 한국 시간으로 변환
+    timeZone: 'Asia/Seoul',
   }
 
   const optionsTime: Intl.DateTimeFormatOptions = {
@@ -16,7 +20,9 @@ export default function FormatDate(dateString: string) {
     minute: '2-digit',
     hour12: false,
   }
+
   const day = date.toLocaleDateString('ko-KR', optionsDate)
   const time = date.toLocaleTimeString('ko-KR', optionsTime)
+
   return `${day}, ${time}`
 }
