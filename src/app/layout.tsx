8032debfb67f793b423cdbe37aad280d3d6a26cd
wjdsheep15 <wjd15sheep@gmail.com>
2024-10-14 16:11:26 +0900

    fix layout import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer/Footer'
 import Providers from '@/components/Providers'
-import { startCronJob } from '@/lib/cron'
 
 const inter = Inter({ subsets: ['latin'] })
 
