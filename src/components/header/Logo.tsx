cbcb689a6d14077014654560a7f8c1d886981b8c
wjdsheep15 <wjd15sheep@gmail.com>
2024-10-23 00:54:53 +0900

    feat favicon.ico add

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Logo() {
   return (
-    <div className='h-12 w-24 bg-gray-400 text-white'>
-      <Link href='/'>LOGO</Link>
+    <div className='flex items-center text-white'>
+      <Link href='/'>
+        <Image
+          src={'/logo/favicon.png'}
+          alt='Logo 이미지'
+          width={35}
+          height={35}
+          style={{ width: 'auto', height: 'auto' }}
+        ></Image>
+      </Link>
     </div>
   )
 }
--- src/components/header/NaviBar.tsx
+++ src/components/header/NaviBar.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link'
 
 export default function NaviBar() {
   return (
-    <div className='flex h-12 w-auto items-center gap-4 text-base text-gray-500 sm:text-lg'>
+    <div className='flex h-12 w-auto items-center gap-8 sm:text-lg'>
       <Link href={'/'}>
-        <span className='hover:text-blue-500'>메인</span>
+        <span className='text-xl hover:text-blue-500'>메인</span>
       </Link>
-      <span> |</span>
+      <span>|</span>
 
       <Link href={'/raidguide'}>
-        <span className='hover:text-blue-500'>레이드 공략</span>
+        <span className='text-xl hover:text-blue-500'>레이드 공략</span>
       </Link>
-      <span> |</span>
+      <span>|</span>
       <Link href={'/mypage'}>
-        <span className='hover:text-blue-500'>마이페이지</span>
+        <span className='text-xl hover:text-blue-500'>마이페이지</span>
       </Link>
     </div>
   )
