2e3d2a1dbcaa5221255699c27c1869b4f9ac3d2b
wjdsheep15 <wjd15sheep@gmail.com>
2024-10-30 21:09:34 +0900

    fix myinfoField

--- src/components/mypageField/myInfoComponent.tsx
+++ src/components/mypageField/myInfoComponent.tsx
@@ -1,5 +1,5 @@
 interface Props {
-  userId: string
+  myInfoData: MyinfoFetch
 }
 
 interface MyinfoFetch {
@@ -14,45 +14,22 @@ interface MyinfoFetch {
   character_count: number
 }
 
-const myInfoFetch = async (userId: string) => {
-  try {
-    const response = await fetch(`${process.env.API_URL}/api/mypageMyInfoGet?user_id=${userId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      next: { tags: ['myInfo'] },
-    })
-    const data = await response.json()
-    if (response.ok) {
-      return data.postRows[0]
-    } else {
-      return []
-    }
-  } catch (error) {
-    console.error('myInfoFetch 에러')
-  }
-  return []
-}
-
-export default async function MyInfoComponent({ userId }: Props) {
-  const dataRow = userId.length > 0 ? await myInfoFetch(userId) : null
-
+export default async function MyInfoComponent({ myInfoData }: Props) {
   return (
     <div className='flex w-full flex-col rounded-md border p-4 shadow-lg'>
       <h1 className='text-lg'>• 내 정보</h1>
 
       <div className='flex flex-col sm:flex-row'>
         <div className='flex flex-col text-base sm:basis-1/2'>
-          <span className='mt-2'>아이디 : {dataRow.user_id}</span>
-          <span>등급 : {dataRow.role}</span>
-          <span>등록된 캐릭터 수 : {dataRow.character_count}</span>
-          <span>닉네임 : {dataRow.nickname}</span>
+          <span className='mt-2'>아이디 : {myInfoData.user_id}</span>
+          <span>등급 : {myInfoData.role}</span>
+          <span>등록된 캐릭터 수 : {myInfoData.character_count}</span>
+          <span>닉네임 : {myInfoData.nickname}</span>
         </div>
         <div className='flex flex-col text-base sm:basis-1/2'>
-          <span>개설 현황 : {dataRow.raid_posts_count}</span>
-          <span>신청 현황 : {dataRow.applicants_count}</span>
-          <span>승인 수 : {dataRow.applicants_approval}</span>
+          <span>개설 현황 : {myInfoData.raid_posts_count}</span>
+          <span>신청 현황 : {myInfoData.applicants_count}</span>
+          <span>승인 수 : {myInfoData.applicants_approval}</span>
         </div>
       </div>
     </div>
--- src/components/mypageField/myInfoField.tsx
+++ src/components/mypageField/myInfoField.tsx
@@ -5,15 +5,60 @@ import { authOptions } from '@/auth'
 import MyInfoComponent from '@/components/mypageField/myInfoComponent'
 import MyInfoNickName from '@/components/mypageField/myInfoNickName'
 
+interface MyinfoFetch {
+  user_id: string
+  user_name: string
+  birthday: number
+  nickname: string
+  role: string
+  applicants_count: number
+  applicants_approval: number
+  raid_posts_count: number
+  character_count: number
+}
+
+const myInfoFetch = async (userId: string) => {
+  try {
+    const response = await fetch(`${process.env.API_URL}/api/mypageMyInfoGet?user_id=${userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      next: { tags: ['myInfo'] },
+    })
+    const data = await response.json()
+    if (response.ok) {
+      return data.postRows[0]
+    } else {
+      return []
+    }
+  } catch (error) {
+    console.error('myInfoFetch 에러')
+  }
+  return []
+}
+
 export default async function MyInfoField() {
   const session = await getServerSession(authOptions)
   let userId = ''
+  let myInfoData: MyinfoFetch = {
+    user_id: '',
+    user_name: '',
+    birthday: 0,
+    nickname: '',
+    role: '',
+    applicants_count: 0,
+    applicants_approval: 0,
+    raid_posts_count: 0,
+    character_count: 0,
+  }
   if (session && session.user.id) {
     userId = session.user.id
+    myInfoData = await myInfoFetch(userId)
   }
   return (
     <div className='flex w-full flex-col items-center justify-center gap-4 sm:mt-8'>
-      <MyInfoComponent userId={userId} />
+      <MyInfoComponent myInfoData={myInfoData} />
       <MyInfoNickName userId={userId} />
       <PasswordChange userId={userId} />
       <UserDelete userId={userId} />
