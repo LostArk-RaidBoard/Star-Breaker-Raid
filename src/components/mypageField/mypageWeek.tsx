f0799f3382acc89fbecea80e066fa3e5f163e489
wjdsheep15 <wjd15sheep@gmail.com>
2025-01-10 16:34:37 +0900

    fix log time

--- src/components/mypageField/mypageWeek.tsx
+++ src/components/mypageField/mypageWeek.tsx
@@ -95,6 +95,8 @@ export default function MypageWeek({ weekSchedule, userId }: Props) {
                 // 현재 날짜
                 const today = toZonedTime(new Date(), timeZone)
                 console.log('=================')
+                console.log('아이템 : ' + item.schedule_time)
+                console.log('raidTime : ' + raidTime)
                 console.log('riadDat : ' + raidDate)
                 console.log('riadDat getDate : ' + raidDate.getDate())
                 console.log('today :' + today.toString())
