e61865de2d8f7a5915e9f1f186b523013bf265ba
wjdsheep15 <wjd15sheep@gmail.com>
2025-02-16 13:31:56 +0900

    fix cached

--- src/components/raidGuideField/RaidGuideVideoPlayer.tsx
+++ src/components/raidGuideField/RaidGuideVideoPlayer.tsx
@@ -87,7 +87,7 @@ export default function RaidGuideVideoPlayer({ raidYoutubeVideoId }: Props) {
       >
         <YouTubeEmbed
           videoid={raidYoutubeVideoId?.[userChosie] || ''}
-          params='controls=0'
+          params='controls=1'
           style='position: absolute; top: 0; left: 0; width: 100%; height: 100%; max-width: none;'
         />
       </div>
