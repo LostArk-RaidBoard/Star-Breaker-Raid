efda346e41219a8a93f6ac957d243dca08cea83d
wjdsheep15 <wjd15sheep@gmail.com>
2025-12-15 00:47:09 +0900

    fix raid post delete schedule

--- src/components/schedulePage/SchedulePage.tsx
+++ src/components/schedulePage/SchedulePage.tsx
@@ -1,36 +1,8 @@
 import { auth } from '@/auth'
 import ServerLevelCharacterSorter from '@/components/utils/ServerLevelCharacterSorter'
-import MyApplications from '@/components/schedulePage/MyApplications'
 import RaidWeekPlanner from '@/components/schedulePage/RaidWeekPlanner.'
-import { converToKoranTime1 } from '@/components/utils/converToKoreanTime'
-import ManageCreatedPosts from '@/components/schedulePage/ManageCreatedPosts'
 
 import React from 'react'
-
-interface RaidPost {
-  post_id: number
-  raid_name: string
-  raid_time: string
-  character_name: string
-  character_classicon: string
-  raid_level: string
-  approval: boolean
-  nickname: string
-}
-
-interface RaidPostCreate {
-  post_id: number
-  raid_name: string
-  raid_time: string
-  limit_level: number
-  user_id: string
-  character_name: string
-  raid_limitperson: number
-  character_classicon: string
-  raid_level: string
-  approval: number
-  rejected_count: number
-}
 
 interface Schedule {
   schedule_id: number
@@ -48,60 +20,6 @@ interface CharacterName {
   character_name: string
   character_level: string
   server_name: string
-}
-
-const applicationPostGetHandler = async (userId: string) => {
-  try {
-    const response = await fetch(
-      `${process.env.API_URL}/api/scheduleAPI/scheduleAppliactionPostGet?user_id=${userId}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        next: { tags: ['applicationTage'] },
-      },
-    )
-    const data = await response.json()
-    if (response.ok && response.status === 200) {
-      return data.postRows.map((item: RaidPost) => {
-        item.raid_time = converToKoranTime1(item.raid_time)
-        return item
-      })
-    } else {
-      return []
-    }
-  } catch (error) {
-    console.error(error)
-    return []
-  }
-}
-
-const createPostGetHandler = async (userId: string) => {
-  try {
-    const response = await fetch(
-      `${process.env.API_URL}/api/raidPostAPI/createPost?user_id=${userId}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        next: { tags: ['createPostTage'] },
-      },
-    )
-    const data = await response.json()
-    if (response.ok && response.status === 200) {
-      return data.postRows.map((item: RaidPostCreate) => {
-        item.raid_time = converToKoranTime1(item.raid_time)
-        return item
-      })
-    } else {
-      return []
-    }
-  } catch (error) {
-    console.error(error)
-    return []
-  }
 }
 
 const weekScheduleGetHandler = async (userId: string) => {
@@ -140,15 +58,10 @@ const weekScheduleGetHandler = async (userId: string) => {
 export default async function SchedulePageField() {
   const session = await auth()
   let userId = ''
-  let applicationPostGet: RaidPost[] = []
-  let createPostGet: RaidPostCreate[] = []
   let weekSchedule: Schedule[] = []
   let characterName: CharacterName[] = []
 
   if (session && session.user.id) {
-    applicationPostGet = await applicationPostGetHandler(session.user.id)
-    createPostGet = await createPostGetHandler(session.user.id)
-
     const { weekSchedule: scheduleData, characterName: characterData } =
       await weekScheduleGetHandler(session.user.id)
     weekSchedule = scheduleData
@@ -159,10 +72,6 @@ export default async function SchedulePageField() {
   return (
     <div className='flex w-full flex-col'>
       <RaidWeekPlanner weekSchedule={weekSchedule} userId={userId} characterName={characterName} />
-      <div className='mt-4 flex flex-col gap-4 md:flex-row'>
-        <MyApplications userId={userId} applicationPostGet={applicationPostGet} />
-        <ManageCreatedPosts createPostGet={createPostGet} />
-      </div>
     </div>
   )
 }
